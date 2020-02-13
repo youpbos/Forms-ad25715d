7f06bbd43d1932d9367a25efa93d294367c23bd0
youp bos <2107418@talnet.nl>
2020-02-13 13:45:09 +0100

    Uitwerking van de exercise

---
+++ form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <input placeholder="Jouw E-mail adress" type="email" name="" id="">
+    <button type="submit">verstuur</button>
+</body>
+
+</html>
